afad0cec1de79dffa1f053ba11bd413fe961f5b5
WeiJie <urink8854@gmail.com>
2016-08-03 18:03:43 +0800

    commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,7 @@
     <div id="wrapper">
         <!-- container section start -->
         <section id="container">
-            @include('layouts.partials.nevbar')
+        @include('layouts.partials.nevbar')
             <section id="main-content">
                 <section class="wrapper">
                     <div style="min-height:750px;">
@@ -17,44 +17,10 @@
                     </div>
                 </section>
             </section>
+        @include('layouts.partials.footer')
         </section>
     </div>
-    <!-- Modal -->
-    <div class="modal fade" id="AddForm" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">新增</h4>
-                </div>
-                @section('AddForm') @show
-            </div>
-        </div>
-    </div>
-    <!-- Modal -->
-    <div class="modal fade" id="EditForm" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">編輯</h4>
-                </div>
-                @section('EditForm') @show
-            </div>
-        </div>
-    </div>
 
-    <!-- Modal -->
-    <div class="modal fade bs-example-modal-sm" id="DelForm" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-sm">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">你確定要移除嗎？</h4>
-                </div>
-                @section('DelForm') @show
-            </div>
-        </div>
-    </div>
-    @include('layouts.partials.footer')
-    <!-- container section start -->
 </body>
 
 </html>
