f4cabeb05451785ef5a781ca57aefdd03c3b3a10
WeiJie <urink8854@gmail.com>
2016-08-04 08:34:48 +0800

    commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,6 @@
         background-color: lightblue;
     }
 </style>
-
 <body>
     <div id="wrapper">
         <!-- container section start -->
@@ -14,46 +13,13 @@
                 <section class="wrapper">
                     <div style="min-height:750px;">
                         @yield('content')
+                        @include('layouts.partials.modalForm')
                     </div>
                 </section>
             </section>
+        @include('layouts.partials.footer')
         </section>
     </div>
-    <!-- Modal -->
-    <div class="modal fade" id="AddForm" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">新增</h4>
-                </div>
-                @section('AddForm') @show
-            </div>
-        </div>
-    </div>
-    <!-- Modal -->
-    <div class="modal fade" id="EditForm" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">編輯</h4>
-                </div>
-                @section('EditForm') @show
-            </div>
-        </div>
-    </div>
-
-    <!-- Modal -->
-    <div class="modal fade bs-example-modal-sm" id="DelForm" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-sm">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">你確定要移除嗎？</h4>
-                </div>
-                @section('DelForm') @show
-            </div>
-        </div>
-    </div>
-    @include('layouts.partials.footer')
     <!-- container section start -->
 </body>
 
