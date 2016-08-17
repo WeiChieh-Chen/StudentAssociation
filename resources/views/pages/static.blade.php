3b94695c3c585e431fd28d1ddadd705aee5f9872
Controller <Controller@Controler-HP>
2016-08-17 17:10:39 +0800

    commit

--- resources/views/pages/static.blade.php
+++ resources/views/pages/static.blade.php
@@ -14,6 +14,7 @@
         }
     }
 ?>
+
 <canvas id="chart"></canvas>
 <script>
     var ctx = document.getElementById("chart");
@@ -24,12 +25,43 @@
             datasets : [{
                 label: '投票人數',
                 data:[{{implode(',', $getVotes)}}],
-                backgroundColor: "rgb(0,0,89)",
-                 borderWidth: 10
-                
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                 ],
+                borderWidth: 1        
             }],
 
         },options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            },
+            legend: {
+                display: true,
+                labels: {
+                    fontColor: 'black'
+                }
+            },
+            margins: {
+                left: 20,
+                right: 20,
+            }
         }
     });
 </script>
