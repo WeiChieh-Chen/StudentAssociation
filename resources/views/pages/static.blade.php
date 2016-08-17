8165d351c169d0a19fb379c2a65a5df36fca8413
WeiJie <urink8854@gmail.com>
2016-08-17 19:33:38 +0800

    圓餅圖的完成

--- resources/views/pages/static.blade.php
+++ resources/views/pages/static.blade.php
@@ -1,7 +1,4 @@
-@extends('layouts.master')
-@section('title','投票區')
-@section('pagename',$get->item)
-@section('content')
+@extends('layouts.master') @section('title','投票區') @section('pagename',$get->item) @section('content')
 <?php
 
     $getItems = array();
@@ -14,26 +11,26 @@
         }
     }
 ?>
-
-<canvas id="chart"></canvas>
-<script>
+    <canvas id="chart"></canvas>
+    <script>
+        console.log('抱歉! 直接顯示在圓餅圖上的%數，我想不出作法!!!');
     var ctx = document.getElementById("chart");
     var myNewChart = new Chart(ctx ,{
-        type: 'bar',
+        type: 'pie', //bar
         data: {
             labels : ['{!!implode("','",$getItems)!!}'],
-            datasets : [{
+            datasets : [{ 
                 label: '投票人數',
                 data:[{{implode(',', $getVotes)}}],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(255, 206, 86, 0.5)',
+                    'rgba(75, 192, 192, 0.5)',
+                    'rgba(153, 102, 255, 0.5)',
+                    'rgba(255, 159, 64, 0.5)'
                 ],
-                borderColor: [
+                hoverBackgroundColor: [
                     'rgba(255,99,132,1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
@@ -43,26 +40,28 @@
                  ],
                 borderWidth: 1        
             }],
-
         },options: {
-            scales: {
+            {{-- scales: {
                 yAxes: [{
+                    reverse: true,
                     ticks: {
                         beginAtZero:true
                     }
                 }]
-            },
-            legend: {
-                display: true,
-                labels: {
-                    fontColor: 'black'
+            }, --}}
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        var dataset = data.datasets[tooltipItem.datasetIndex];
+                        var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+                            return previousValue + currentValue;
+                        });
+                        var currentValue = dataset.data[tooltipItem.index];
+                        var precentage = Math.floor(((currentValue/total) * 100)+0.5);     
+                    return precentage + "%, 共得 "+currentValue+" 票";
+                    }
                 }
-            },
-            margins: {
-                left: 20,
-                right: 20,
             }
         }
     });
-</script>
-@endsection
+</script> @endsection
