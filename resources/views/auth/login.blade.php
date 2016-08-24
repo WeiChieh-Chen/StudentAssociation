edc8d5cc2173637d391efe3a75e48ad154a6d947
LihaoHuang <azxaxa@gmail.com>
2016-08-24 10:56:09 +0800

    加入 placeholder 提示

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
                                 <label for="username" class="col-md-4 control-label">使用者帳號 : </label>
 
                                 <div class="col-md-6">
-                                    <input id="username" type="username" class="form-control" name="username" value="{{ old('username') }}">
+                                    <input id="username" type="username" class="form-control" name="username" value="{{ old('username') }}" placeholder="例如 : 40123456">
 
                                     @if ($errors->has('username'))
                                         <span class="help-block">
@@ -31,7 +31,7 @@
                                 <label for="password" class="col-md-4 control-label">密碼 : </label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password">
+                                    <input id="password" type="password" class="form-control" name="password" placeholder="請輸入AD密碼">
 
                                     @if ($errors->has('password'))
                                         <span class="help-block">
